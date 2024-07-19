d650d19ce4bbc6ed1a8e99a036fbdbd0bda9e4ed
ZEK <ZEK@DESKTOP-MQI8H1N>
2024-07-19 12:00:27 +0300

    Proje dosyası ekle.

---
+++ whilevedowihile/Program.cs
@@ -0,0 +1,26 @@
+﻿//bu istenilen örneğin while ile yapılmış hali
+int sayac = 0;
+
+Console.Write("Lütfen Bir Limit Değeri Girin : ");
+int limit = Convert.ToInt32(Console.ReadLine());
+
+while (sayac <= limit)
+{
+    Console.WriteLine("Ben bir Patika'lıyım");
+    sayac++;
+}
+
+// bu da istenilen örneğin dowhile ile yapılmış hali
+
+//int sayac = 0;
+
+//Console.Write("Lütfen Bir Limit Değeri Girin : ");
+//int limit = Convert.ToInt32(Console.ReadLine());
+
+//do
+//{
+//    Console.WriteLine("Ben bir Patika'lıyım");
+//    sayac++;
+//} while (sayac <= limit);
+
+
